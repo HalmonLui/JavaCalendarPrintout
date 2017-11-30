d10643fed10e67b848526be4c2ad0d9833d956f0
HalmonLui <halmonlui@gmail.com>
2017-11-30 09:58:18 -0500

    Minor Fix
    Changed titles

--- CalendarPrintout.java
+++ CalendarPrintout.java
@@ -1,28 +1,6 @@
-/*********************************************************************
-*	Lab 8 - PA7a
-*	YOUR NAME HERE
-*	COMP1000-XX  (ENTER YOUR SESSION, EITHER 05/06 or 07/08)
-*	XX/XX/2016  (UPDATE THE DATE) 
-*
-**********************************************************************
-*	Problem Description
-*
-*	Write a program that reads two numbers from the user and
-*	prints out the sum of those two numbers. (UPDATE THE DESCRIPTION)
-*
-***********************************************************************
-*	Analysis
-*
-*	Inputs:  Two integers, read from the user.
-*
-*	Outputs: The sum of the inputs.
-*
-**********************************************************************/
-
-package edu.wit.cs.comp1000;
 import java.util.Scanner;
 
-public class PA7a {
+public class CalendarPrintout {
 	
 	/**
 	 * Error to output if year is not positive
